f1143c6bb9b9b6a7db637714e4653e833580e93a
skynaitil <skytanglitian@gmail.com>
2018-11-03 21:42:24 +0800

    Add user info to main app.

--- pocaims/src/App.js
+++ pocaims/src/App.js
@@ -1,25 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from 'react-dom';
+import {Link} from 'react-router-dom';
+import axios from 'axios';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      users: []
+    };
+  }
+
+  componentDidMount(){
+    axios.get('/api/user'
+    .then(res => {
+      this.setState({users: res.data});
+      console.log(this.state.users);
+    }))
+  }
+
+  render(){
+    return(
+      <div class="container">
+      <div class="panel panel-default">
+      <div class="panel-heading">
+      <h3 class="panel-title">
+        USER LIST
+      </h3>
+      </div>
+      
+      <div class="panel-body">
+      <h4><Link to="/create"><span class="glyphicon glyphicon-plus-sign" aria-hidden="true"></span>Add User</Link></h4>
+      <table class="table table-stripe">
+      <thead>
+        <tr>
+          <th>Role</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {this.state.users.map(user=>
+        <tr>
+          <td><Link to={`/show/${user._id}`}>{user.role}</Link></td>
+          <td>{user.firstName}</td>
+          <td>{user.lastName}</td>
+        </tr>
+        )}
+      </tbody>
+      </table>
+      </div>
+      </div>
       </div>
     );
   }
