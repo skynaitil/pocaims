34269143ad049b7a4a1abc168996bccb3a19c1ca
skynaitil <skytanglitian@gmail.com>
2018-11-03 23:21:31 +0800

    Resolve build issue.

--- pocaims/src/index.js
+++ pocaims/src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router, Router} from 'react-router-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../node_modules/bootstrap/dist/css/bootstrap-theme.min.css';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWork';
+import registerServiceWorker from './registerServiceWorker';
 import Edit from './components/Edit';
 import Create from './components/Create';
 import Show from './components/Show';
