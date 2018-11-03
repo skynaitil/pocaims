8a729c9e2b1f93945bb1139b21d2cbdaa14f6ed8
skynaitil <skytanglitian@gmail.com>
2018-11-03 21:26:09 +0800

    Configure register serviceWorker.

--- pocaims/src/index.js
+++ pocaims/src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Router} from 'react-router-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/css/bootstrap-theme.min.css';
 import './index.css';
 import App from './App';
+import registerServiceWork from './registerServiceWork';
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
