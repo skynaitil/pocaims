b02651f219bbda64f52ff084b8b46221db5e4c4d
skynaitil <skytanglitian@gmail.com>
2018-11-03 21:30:37 +0800

    Added Route path.

--- pocaims/src/index.js
+++ pocaims/src/index.js
@@ -5,13 +5,29 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap/dist/css/bootstrap-theme.min.css';
 import './index.css';
 import App from './App';
-import registerServiceWork from './registerServiceWork';
+import registerServiceWorker from './registerServiceWork';
+import Edit from './components/Edit';
+import Create from './components/Create';
+import Show from './components/Show';
 
-import * as serviceWorker from './serviceWorker';
+ReactDOM.render(
+    <Router>
+        <div>
+            <Route exact path='/' component={App}/>
+            <Route path='/edit/:id' component={Edit}/>
+            <Route path='/create' component={Create}/>
+            <Route path='/show/:id' component={Show}/>
+        </div>
+    </Router>,
+    document.getElementById('root')
+);
+registerServiceWorker();
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+//import * as serviceWorker from './serviceWorker';
+//ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//serviceWorker.unregister();
