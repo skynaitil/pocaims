fd09a52eb3b7d1bb4a3096c393b10234a14e133c
skynaitil <skytanglitian@gmail.com>
2018-11-03 20:47:33 +0800

    Added user schema for MongoDB.

--- pocaims/models/User.js
+++ pocaims/models/User.js
@@ -1,1 +1,15 @@
 var mongoose = require('mongoose');
+
+var UserSchema = new mongoose.Schema({
+    id: String,
+    firstName: String,
+    lastName: String,
+    password: String,
+    email: String,
+    role: String,
+    telNumber: Double,
+    validUser: Boolean,
+    admin: Boolean
+});
+
+module.exports = mongoose.model('User', UserSchema);
